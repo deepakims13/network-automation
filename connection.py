aeb6b939fa1434c95286223b387650bc7e71a971
deepakims13 <118072257+deepakims13@users.noreply.github.com>
2022-11-14 00:09:44 +0530

    Update connection.py

--- connection.py
+++ connection.py
@@ -1,8 +1,3 @@
-from netmiko import ConnectHandler
-try:
-    device = ConnectHandler(device_type='cisco_ios', ip='192.168.255.249', username='cisco', password='cisco')
-    output = device.send_command("show version")
-    print (output)
-except Exception as error_message:
-    print("Unable to connect")
-    print(error_message)
+import paramiko
+device = paramiko.SSHClient()
+
