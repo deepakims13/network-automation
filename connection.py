2f8007b75dcaddb0f90d6fba92825128cb1dbed2
deepakims13 <118072257+deepakims13@users.noreply.github.com>
2022-11-14 00:53:37 +0530

    Update connection.py

--- connection.py
+++ connection.py
@@ -1,3 +1,10 @@
 import paramiko
+
 device = paramiko.SSHClient()
+
+# Load SSH host keys.
+device.load_system_host_keys()
+
+# Add SSH host key when missing.
+device.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
