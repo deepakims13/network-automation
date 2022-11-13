3a3dc1322f25eaa9fa6e0be6bc58e0b530ed59dc
deepakims13 <118072257+deepakims13@users.noreply.github.com>
2022-11-14 00:31:53 +0530

    Update bgp_script.py

--- bgp_script.py
+++ bgp_script.py
@@ -1,1 +1,13 @@
 import connection
+import json
+import pytest
+
+def setup_module():
+    '''
+    Setup module for common configurations
+    '''
+    # Opening JSON file
+    with open('bgp_input.json') as bgp_input:
+    # returns JSON object as 
+    # a dictionary
+    data = json.load(bgp_input)
