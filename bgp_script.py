180cecf79de5d1268ac8b429a70cec8bac7f806e
deepakims13 <118072257+deepakims13@users.noreply.github.com>
2022-11-14 00:45:06 +0530

    Update bgp_script.py

--- bgp_script.py
+++ bgp_script.py
@@ -1,6 +1,7 @@
 import connection
 import json
 import pytest
+import logging
 
 def setup_module():
     '''
@@ -11,3 +12,18 @@ def setup_module():
     # returns JSON object as 
     # a dictionary
     data = json.load(bgp_input)
+    
+    try:
+        # Connect to router1 using username/password authentication.
+        connection.device.connect(router_ip=data['R1']['router_ip'], 
+                    username=data['R1']['router_username'], 
+                    password=data['R1']['router_password'],
+                    look_for_keys=False )
+        # Connect to router2 using username/password authentication.
+        connection.device.connect(router_ip=data['R2']['router_ip'], 
+                    username=data['R2']['router_username'], 
+                    password=data['R2']['router_password'],
+                    look_for_keys=False )
+    except Exception as error_message:
+        log.info("Unable to connect")
+        log.info(error_message)
