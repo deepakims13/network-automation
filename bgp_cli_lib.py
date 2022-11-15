b741c6cbff8d4d78a867c90b01f738abc1684f5c
deepakims13 <118072257+deepakims13@users.noreply.github.com>
2022-11-15 22:29:39 +0530

    Update bgp_cli_lib.py

--- bgp_cli_lib.py
+++ bgp_cli_lib.py
@@ -1,3 +1,22 @@
 import logging
 
-def setup_router_interface(self, interface_name, netmask)
+def setup_router_interface(self, interface_name, ip, netmask):
+    '''
+    Configure router interface.
+  
+    Extended description of function.
+  
+    Parameters:
+    interface_name (str): Name of interface
+    ip (str): Ip address
+    netmask (str): Ip address netmask
+  
+    Returns:
+    List: List of interface config
+    # Create a CLI configuration
+    interface_config = [
+        "interface {}".format(interface_name),
+        "ip address {} {}".format(ip, netmask),
+        "no shut"
+    ]
+    return interface_config
