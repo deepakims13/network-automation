c516069a43131775da2195d9b7c6b1bb144e71f0
deepakims13 <118072257+deepakims13@users.noreply.github.com>
2022-11-15 22:55:51 +0530

    Update bgp_script.py

--- bgp_script.py
+++ bgp_script.py
@@ -2,6 +2,7 @@ import connection
 import json
 import pytest
 import logging
+from connection import netmiko_connection
 
 def setup_module():
     '''
@@ -27,3 +28,20 @@ def setup_module():
     except Exception as error_message:
         log.info("Unable to connect")
         log.info(error_message)
+
+class TestInterfaceConfig():
+    '''
+    Testcase for Interface configurations in router
+    '''
+    def test_interface_config():
+        conn_setup_R1 = netmiko_connection(ip_address=data['R1']['router_ip'], 
+                                        ssh_port=data['R1']['ssh_port'], 
+                                        username=data['R1']['router_username'],
+                                        password=data['R1']['router_password'], 
+                                        device_type=data['R1']['device_type'])
+        conn_setup_R2 = netmiko_connection(ip_address=data['R2']['router_ip'], 
+                                        ssh_port=data['R2']['ssh_port'], 
+                                        username=data['R2']['router_username'],
+                                        password=data['R2']['router_password'], 
+                                        device_type=data['R2']['device_type'])
+        
