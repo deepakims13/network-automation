cfbba9a0651d78b99a438e7ddf1fba165d8bcf30
deepakims13 <118072257+deepakims13@users.noreply.github.com>
2022-11-16 15:15:05 +0530

    Update connection.py

--- connection.py
+++ connection.py
@@ -9,7 +9,7 @@ device.load_system_host_keys()
 # Add SSH host key when missing.
 device.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-def netmiko_connection(self, ip_address, ssh_port, username, password, device_type):
+def netmiko_connection(ip_address, ssh_port, username, password, device_type):
     # Open CLI connection to device
     conn_obj = ConnectHandler(ip = ip_address,
                          port = ssh_port,
